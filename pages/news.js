895cf1cf6125aba3239fde5bd16d2e019d1b03c8
Marc Medhat <marcmedhat6211@gmail.com>
2022-06-08 18:56:38 +0200

    enable routing by just adding 2 files

---
+++ pages/news.js
@@ -0,0 +1,10 @@
+// our-domain.com/news
+
+/**
+ * We don't need any imports here because NextJS makes sure that these imports were made behind the scene
+ */
+const NewsPage = () => {
+  return <h1>The News Page</h1>;
+};
+
+export default NewsPage;
